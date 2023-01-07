d151cabc902837aa41d41d06a89c30e07a8fcacc
Elie Soued <eliegebransoued@gmail.com>
2023-01-07 23:06:36 +0200

    landing page first attempt

--- src/App.js
+++ src/App.js
@@ -23,33 +23,36 @@ const App = () => {
                     </div>
 
                     <form>
-                      <div className="form-outline mb-4">
+                      <div>
                         <input
                           type="email"
                           id="form2Example11"
                           style={{ border: "1px solid lightgray" }}
                           className="form-control"
-                          placeholder="Phone number or email address"
+                          placeholder="Enter your username"
                         />
-                        <label className="form-label" htmlFor="form2Example11">
-                          Enter username
-                        </label>
+                        <label
+                          className="form-label"
+                          htmlFor="form2Example11"
+                        ></label>
                       </div>
 
-                      <div className="form-outline mb-4">
+                      <div>
                         <input
                           type="password"
                           id="form2Example22"
                           className="form-control"
+                          placeholder="Enter your password"
                         />
-                        <label className="form-label" htmlFor="form2Example22">
-                          Enter Password
-                        </label>
+                        <label
+                          className="form-label"
+                          htmlFor="form2Example22"
+                        ></label>
                       </div>
 
                       <div className="text-center pt-1 mb-5 pb-1">
                         <button
-                          className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
+                          className="btn btn-primary btn-block fa-lg  mb-3"
                           type="button"
                         >
                           Log in
@@ -71,16 +74,24 @@ const App = () => {
                     </form>
                   </div>
                 </div>
-                <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
+                <div className="rounded-top col-lg-6 d-flex align-items-center gradient-custom-2">
                   <div>
-                    <img className="w-100" src={responsability}></img>
+                    <img
+                      className="w-100"
+                      src={responsability}
+                      style={{ borderRadius: "5px" }}
+                    ></img>
                   </div>
                 </div>
               </div>
             </div>
-            <footer className="col-xl-12">
-              <img className="w-100" src={undersurface}></img>
-            </footer>
+            <div className="col-xl-12">
+              <img
+                className="w-100"
+                src={undersurface}
+                style={{ borderRadius: "5px" }}
+              ></img>
+            </div>
           </div>
         </div>
       </div>
